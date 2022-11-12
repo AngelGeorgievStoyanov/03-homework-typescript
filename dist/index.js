815d5375a99a6e58704692598cd34655f8e35d2b
Angel <angelo_stoyanov@abv.bg>
2022-11-12 16:49:46 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const result = document.getElementById('result');
+function init() {
+    return console.log('Hello TypeScript!');
+}
+init();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+
+
+const result= document.getElementById('result')
+
+function init() {
+    return console.log('Hello TypeScript!')
+}
+
+init()
